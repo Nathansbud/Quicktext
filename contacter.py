00948c5e7015bf062cc4740d22103149d6313b77
Zack Amiton <zamiton@outlook.com>
2024-09-24 13:42:23 -0400

    update contact dump script a touch

--- contacter.py
+++ contacter.py
@@ -4,9 +4,17 @@ import pyperclip
 vcard_template = "BEGIN:VCARD\nVERSION:3.0\nFN:{}\nTEL:{}\nEND:VCARD"
 output_dir = os.path.join(os.path.dirname(__file__), "ScriptContacts")
 
+# Replace this with your actual information if you wish to include 
+# your contact in the dump, e.g. ["YOUR_NUMBER", "YOUR_NAME"]
+PERSONAL = []
+
 def run_contacter():
     clipboard = pyperclip.paste()
-    lines = [l.split("@") for l in clipboard.splitlines()]
+    lines = sorted(
+        [l.split("@") for l in clipboard.splitlines()] + [PERSONAL],
+        key=lambda v: v[1]
+    )
+
     with open(os.path.join(output_dir, f"CONTACT-GROUPCHAT.vcf"), "w+") as cf:
         for contact in lines:
             cf.write(vcard_template.format(contact[1].strip(), contact[0].strip()) + "\n")
